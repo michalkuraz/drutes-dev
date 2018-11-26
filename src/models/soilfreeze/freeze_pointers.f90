b9008e777610318b0cb4e5ddbcda4db795266809
miguel <michalkuraz@gmail.com>
2018-11-26 20:55:50 +0100

    in proccess..

--- src/models/soilfreeze/freeze_pointers.f90
+++ src/models/soilfreeze/freeze_pointers.f90
@@ -3,13 +3,44 @@ module freeze_pointers
   
   contains
   
-    subroutine freeze_processes
+    subroutine freeze_processes(processes)
+      use typy
+      use globals
+      
+      integer(kind=ikind), intent(out) :: processes
+      
+      
+      select case (drutes_config%name)
+        case ("freeze")
+          processes = 2
+          
+        case ("LTNE")
+          processes = 4
+          
+        case default
+          print *, "procedure called when unexpected problem name"
+          print *, "exited from freeze_pointers::freeze_processes"
+          error stop
+      end select
+        
     
     end subroutine freeze_processes
   
   
   
-    subroutine frz_pointers
+    subroutine frz_pointers()
+      use typy
+      use globals
+      use global_objs
+      use pde_objs
+      use re_globals
+      use re_constitutive
+      use freeze_globs
+      use freeze_fnc
+      
+      call REstdH(pde(1))
+      
+      
     
     end subroutine frz_pointers
   
