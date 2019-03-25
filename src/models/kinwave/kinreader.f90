732cea4c7f2b15f4d84580ef7a54fa618a0ad4cf
miguel <michalkuraz@gmail.com>
2019-03-25 13:35:27 +0100

    ...

--- src/models/kinwave/kinreader.f90
+++ src/models/kinwave/kinreader.f90
@@ -2,6 +2,13 @@ module kinreader
 
   contains
   
+    subroutine readmesh()
+      use typy
+      
+      
+      open(newunit=filenodes, file="drutes.conf/
     
+    
+    end subroutine readmesh 
 
 end module kinreader
