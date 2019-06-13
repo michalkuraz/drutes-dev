865a182f57706ab611342cde90632bb196a5435d
Juliana Arbelaez Gaviria <jarbelaezga@gmail.com>
2019-06-13 14:22:42 +0200

    extra modifications

--- src/models/RE/re_globals.f90
+++ src/models/RE/re_globals.f90
@@ -60,7 +60,7 @@ module re_globals
   integer, public :: file_evap
   
   !> evaporation input 
-  real(kind=rkind), public :: albedo,latitude,elevation
+  real(kind=rkind), public :: albedo,latitude,elevation, day_year
   
   
 end module re_globals
