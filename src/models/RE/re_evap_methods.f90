7335550b7f954eb711b4192d5a58110f4787f09d
Gustavo <cardenas_castillero@fzp.czu.cz>
2020-05-15 10:38:01 +0200

    Avances del modelo de evap

--- src/models/RE/re_evap_methods.f90
+++ src/models/RE/re_evap_methods.f90
@@ -1,9 +1,31 @@
 module re_evap_methods
-  use typy
+ use typy
+ 
+  
+  public :: McGuinessBorder_fcn
+  public :: makkink_fcn
+  public :: makkinkHasem_fcn
+  public :: DoorenbosPruit_fcn
+  public :: Abtew_fcn
+  public :: PriestleyTaylor_fcn
+  public :: Turc_fcn
+  public :: Hargreaves_fcn
+  public :: HargreaveSamani_fcn
+  public :: JensenHaise_fcn
+  public :: Thornthwaite_fcn
+  public :: Hamon_fcn
+  public :: Linacre_fcn
+  public :: BlaneyCriddle_fcn
+  public :: Oudin_fcn
+  public :: Romanenko_fcn
+  public :: RomanenkOudin_fcn
+  public :: Kharrufa_fcn
+  
   !> if units [m/s] conv_const = 1e-3/86400
   !! if units [mm/day] conv_const = 1.0
   !! if different units find on your own
   !<
+  
   real(kind=rkind), private :: conv_const = 1.0
   contains
 
@@ -17,7 +39,7 @@ module re_evap_methods
       !t: average temperature
       !Rs: solar radiation
       real (kind=rkind) :: val
-      !McGuiness and Border [MJ/m2 day]         
+      !McGuiness and Border [mm/day]         
       val = ((0.0082*t)-0.19)*(Rs/1500)*2.54
       val = val*conv_const
     end function McGuinessBorder_fcn
@@ -29,18 +51,18 @@ module re_evap_methods
       use typy
       real (kind=rkind), intent(in) :: t,Rs
       !x: # of the day in the year 
-      !sl: slope of the saturation vapour pressure curve [mbar/°C]
+      !sl: slope of the saturation vapour pressure curve [kPa/°C]
       !Rs: solar radiation [MJ/m2 day]
-      !Ps: psichromatic constant [mbar/°C]
+      !Ps: psichromatic constant [kPa/°C]
       real (kind=rkind) :: val
       real(kind=rkind) :: sl,Ps,Lh
        !slope of the saturation vapour pressure curve
        sl = 33.8639_rkind*(0.05904_rkind*(0.00738_rkind*(t))+0.8072**7_rkind-0.0000342_rkind)
        !Latent heat [in calories per gram]
        Lh = (5.95_rkind-0.51_rkind*(t))
-       !psicromatic constant [mbar/°C]
+       !psicromatic constant [kPa/°C]
        Ps = (1.013**-3_rkind*101.325_rkind)/(0.622_rkind*Lh)
-      !Makkink Method [MJ/m2 day]         
+      !Makkink Method [mm/day]         
       val = 0.61_rkind*(sl/(sl-Ps))*(Rs/58.5_rkind)-0.012_rkind
     end function makkink_fcn
     
@@ -51,7 +73,7 @@ module re_evap_methods
     use typy
     use core_tools
     real (kind=rkind), intent(in) :: y,z,e,a,Rs,t,t_min,t_max
-    !sl: slope of the saturation vapour pressure curve [mbar/°C]
+    !sl: slope of the saturation vapour pressure curve [kPa/°C]
     !Rs: solar radiation [MJ/m2 day]
     !Ps: psichromatic constant [mbar/°C]
     integer (kind=ikind), intent(in):: x
@@ -61,7 +83,7 @@ module re_evap_methods
       sl = 33.8639*(0.05904*(0.00738*(t))+0.8072**7-0.0000342)
       !Latent heat [in calories per gram]
       Lh = (5.95-0.51*(t))
-      !psicromatic constant [mbar/°C]
+      !psicromatic constant [kPa/°C]
       Ps = (1.013**-3*101.325)/(0.622*Lh)
         !inverse distance Earth-sun [rad]
       dr = 1.0_rkind + 0.033_rkind*cos((2.0_rkind*pi()*x)/365.0_rkind)
@@ -79,10 +101,10 @@ module re_evap_methods
       !Net longwave radiation [MJ/m2 day] 
       R_nl = 4.903e-9*((t_min**4.0_rkind + t_max**4.0_rkind)/2.0_rkind)*(0.34_rkind - & 
                 0.14_rkind*sqrt(e))*(1.35_rkind*(Rs/R_so) -0.35_rkind)
-      !Net radiation [MJ/m2 day]         
+      !Net radiation [mm/day]         
       Rn = R_ns - R_nl
        
-    !Makkink-Hasem Method
+    !Makkink-Hasem Method [mm/day] 
     val = 1.26_rkind*(sl/(sl+Ps))*(Rn/Lh)
     end function makkinkHasem_fcn 
     
@@ -92,23 +114,23 @@ module re_evap_methods
     function DoorenbosPruit_fcn (t,Rs,RH,Ud)  result (val)
      use typy
      real (kind=rkind), intent(in) :: t,Rs,RH,Ud
-     !sl: slope of the saturation vapour pressure curve [mbar/°C]
+     !sl: slope of the saturation vapour pressure curve [kPa/°C]
      !Rs: solar radiation [MJ/m2 day]
-     !Ps: psichromatic constant [mbar/°C]
+     !Ps: psichromatic constant [kPa/°C]
      !b: adjustment factor that varies with mean relative humidity and daytime wind speed
      
      real(kind=rkind) :: val
      real(kind=rkind) :: b,sl,Ps,Lh,a
        b = 0.3
-       !slope of the saturation vapour pressure curve
+       !slope of the saturation vapour pressure curve [kPa/°C]
        sl = 33.8639_rkind*(0.05904_rkind*(0.00738_rkind*(t))+0.8072e7_rkind-0.0000342_rkind)
-       !heat latent
+       !heat latent [kPa/°C]
        Lh = (5.95_rkind-0.51_rkind*(t))
-       !psicromatic constant [mbar/°C]
+       !psicromatic constant [kPa/°C]
        Ps = (1.013e-3_rkind*101.25_rkind)/(0.622_rkind*Lh)
-       !adjustment factor that varies with mean relative humidity and daytime wind speed
+       !adjustment factor that varies with mean relative humidity and daytime wind speed [m/s]
        a = 1.066_rkind-0.13e-2*(RH)+0.045_rkind*(Ud)-0.20e-3*(RH)*(Ud)-0.315e-4*(RH)**2-0.11e-2*(Ud)**2
-     !Doorenbos and Pruit method
+     !Doorenbos and Pruit method [mm/day] 
      val = a*((sl/(sl+Ps))*Rs)+b
     end function DoorenbosPruit_fcn
     
@@ -122,7 +144,7 @@ module re_evap_methods
       real(kind=rkind) :: Lh
       !Latent heat [in calories per gram]
        Lh = 5.95_rkind-0.51_rkind*(t)
-      !Abtew
+      !Abtew [mm/day]
       val = 0.53_rkind*(Rs/Lh)
     end function Abtew_fcn
     
@@ -137,11 +159,13 @@ module re_evap_methods
      a = 1.26
      !slope of the saturation vapour pressure curve
      sl = (33.8639_rkind*(0.05904_rkind*(0.00738_rkind*(t))+0.8072**7_rkind-0.0000342_rkind))
-     !psicromatic constant [mbar/°C]
+     !psicromatic constant [kPa/°C]
      Ps = ((1.013e-3)*10313.25_rkind)/(0.622_rkind*Lh)
      !Latent heat [in calories per gram]
      Lh = (5.95_rkind-0.51_rkind*(t))
-     !soil heat flux density [MJ m-2 day-1]
+     !soil heat flux density [MJm2/day]
+     
+     !Priestley and Taylor [mm/day]
      G = 0.07_rkind*(Tmonth1-Tmonth2)
       val = (a*(sl/(sl+Ps))*((Rs-G)/Lh))
     end function PriestleyTaylor_fcn
@@ -152,12 +176,11 @@ module re_evap_methods
     !> val: Turc
     function Turc_fcn (t,Rs) result (val)
       use typy
-      !> t: input temperature, Rs: solar radiation [m/day]
+      !> t: input temperature [°C], Rs: solar radiation [m/day]
       real (kind=rkind), intent(in) :: t,Rs
       real (kind=rkind) :: val
-    !Turc
     
-    
+    !Turc [mm/day]
      val = 0.013_rkind*(23.88_rkind*(Rs)+ 50_rkind)*(t)*(t+15_rkind)**(-1) 
     end function Turc_fcn
     
@@ -190,8 +213,10 @@ module re_evap_methods
     function Hargreaves_fcn (t,Rs) result (val)
      use typy
      real (kind=rkind), intent(in) :: t,Rs
+     !Temperature [°C]
+     !Solar Radiation [m/day]
      real (kind=rkind) :: val
-    !Hargreaves
+    !Hargreaves [mm/day]
       val = 0.0135_rkind*(t+17.8_rkind)*Rs
     end function Hargreaves_fcn
     
@@ -201,8 +226,12 @@ module re_evap_methods
     function HargreaveSamani_fcn (t,tmax,tmin,Rs) result (val)
      use typy
      real (kind=rkind), intent(in) :: t,tmax,tmin,Rs
+     !t = average temperature [°C]
+     !tmax = maximum temperature [°C]
+     !tmin = minimum temperature [°C]
+     !Rs = Solar radiation [m/day]
      real (kind=rkind) :: val
-    !Turc1
+    !HargreaveSamani [mm/day]
       val = 0.0023_rkind*(tmax-tmin)*0.5_rkind*(t+17.8_rkind)*Rs
     end function HargreaveSamani_fcn
     
@@ -212,20 +241,22 @@ module re_evap_methods
     function JensenHaise_fcn (t,Rs)  result (val)
      use typy
      real (kind=rkind), intent(in) :: t,Rs
+     !Temperature [°C]
+     !Solar Radiation [m/day]
      real (kind=rkind) :: val
      real(kind=rkind) :: e,Lh,Ch,Ct,tx
-     !constant
+     !tx = constant
      tx = -3
-     !Saturation vapour pressure [kPa]
+     !e = Saturation vapour pressure [kPa]
      e = 102.2
-     !Latent heat [in calories per gram]
+     !Lh = Latent heat [in calories per gram]
      Lh = (5.95_rkind-0.51_rkind*(t))
-     !
+     !Ch = Coefficient 
      Ch = 50.0_rkind*(t/e)
      !Coefficient temperature 
      Ct = 1.0_rkind/(27_rkind+7.3_rkind*(Ch))
      
-    !Jensen_Haise
+    !Jensen_Haise [mm/day]
       val = (Ct*(t-tx)*Rs)/Lh
     end function JensenHaise_fcn
     
@@ -236,11 +267,12 @@ module re_evap_methods
     use typy
     use core_tools
     real (kind=rkind), intent(in) :: t
+    !t = average temperature [°C]
     real (kind=rkind) :: val
      real(kind=rkind) :: a_th,I
-    !Monthly heat index
+    !Monthly heat index [-]
     I = (t/5.0_rkind)**1.514
-    !Constant
+    !Constant [-]
     a_th = 0.49239_rkind + 1792e-5*(I) - 771e-2*(I)**2 + 675e-9*(I)**3
     
     !Thornthwaite
@@ -262,7 +294,7 @@ module re_evap_methods
     !Saturated vapor density [g/m3]
     !RHOSAT = 17.30_rkind
     
-    !Thornthwaite
+    !Hamon [mm/day]
     val = 0.1651_rkind*(Ld)*(RHOSAT)*(KPEC)
     end function Hamon_fcn
     
@@ -273,9 +305,13 @@ module re_evap_methods
     function Linacre_fcn (z,t,tmin,Ls) result (val)
     use typy
     real (kind=rkind), intent(in) :: z,t,tmin,Ls
+    !z = elevation (m)
+    !t = average temperature [°C]
+    !tmin = minimum temperature [°C]
+    !Ls = = latitude of the station [rad]
     real (kind=rkind) :: val
     
-    !Linacre
+    !Linacre [mm/day]
     val = (((500_rkind*(t+0.006_rkind+z))/(100.00_rkind-Ls))+(15.00_rkind*(t-tmin)))/(80-t)
     end function Linacre_fcn
     
@@ -288,8 +324,9 @@ module re_evap_methods
     real (kind=rkind), intent(in) :: q,tmin
     real (kind=rkind) :: val
     !q: daily percentage of annual daytime hours
+    !tmin = minimum temperarure [°C]
     
-    !BlaneyCriddle
+    !BlaneyCriddle [mm/day]
     val = (q)*((0.46_rkind)*(tmin) + 8.00_rkind)
     end function BlaneyCriddle_fcn
     
@@ -299,9 +336,11 @@ module re_evap_methods
     function Oudin_fcn (Rs,t) result (val)
     use typy
     real (kind=rkind), intent(in) :: Rs,t
+    !Rs = solar radiation [m/day]
+    !t = average temperature [°C]
     real (kind=rkind) :: val
     
-    !Oudin
+    !Oudin [mm/day]
     val = 0.408_rkind*Rs*(t+5.00_rkind)
     end function Oudin_fcn
     
@@ -311,9 +350,11 @@ module re_evap_methods
     function Romanenko_fcn (RH,t) result (val)
     use typy
     real (kind=rkind), intent(in) :: RH,t
+    !RH = realtive humidity [-]
+    !t = average temperature [°C]
     real (kind=rkind) :: val
     
-    !Romanenko
+    !Romanenko [mm/day]
     val = 0.0018_rkind*(25_rkind + t)**2*(100_rkind - RH)
     end function Romanenko_fcn
     
@@ -335,9 +376,11 @@ module re_evap_methods
     function Kharrufa_fcn (p,t) result (val)
     use typy
     real (kind=rkind), intent(in) :: p,t
+    !p = % of total daytime hours [-]
+    !t = average temperature [°C]
     real (kind=rkind) :: val
     
-    !Kharrufa
+    !Kharrufa [mm/day]
     val = 0.34_rkind*(p)*(t)**1.3
     end function Kharrufa_fcn
 
